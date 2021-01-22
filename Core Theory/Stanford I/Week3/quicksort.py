d39544b94cbed8367f0dc6afce8aeb1b8ea126aa
Omar <omarmuhammed1998.om@gmail.com>
2021-01-22 22:29:12 +0200

    implemented quick sort without choosing pivot subroutine

--- Core Theory/Stanford I/Week3/quicksort.py
+++ Core Theory/Stanford I/Week3/quicksort.py
@@ -1,19 +1,28 @@
 def quicksort(array, left=None, right=None):
+    if len(array) == 1: return array
     if left is None:
         left = 0
     if right is None:
         right = len(array) - 1
+    if not left < right: return
 
     def partition(array, left, right):
-        pivot = array[left]
         i = left + 1
-        for j in range(left, right + 1):
+        print('left: ', left, 'right: ', right)
+        pivot = array[left]
+        for j in range(left + 1, right + 1):
             if array[j] < pivot:
                 array[j], array[i] = array[i], array[j]
                 i += 1
         array[left], array[i - 1] = array[i - 1], array[left]
+        return i - 1
+        
+        
 
-    partition(array, left, right)
+    pivot_index = partition(array, left, right)
+    print(array)
+    quicksort(array, left, pivot_index-1)
+    quicksort(array, pivot_index+1, right)
     return array
 
 
