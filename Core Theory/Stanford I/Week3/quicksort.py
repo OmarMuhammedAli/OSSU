c735d8315909c8cf850a64684d27154cf0e42f2c
Omar <omarmuhammed1998.om@gmail.com>
2021-01-28 00:50:26 +0200

    added median of 3 algorithm to enhance pivot choice

--- Core Theory/Stanford I/Week3/quicksort.py
+++ Core Theory/Stanford I/Week3/quicksort.py
@@ -1,33 +1,73 @@
+
+
 def quicksort(array, left=None, right=None):
     if len(array) == 1: return array
     if left is None:
         left = 0
     if right is None:
         right = len(array) - 1
-    if not left < right: return
+    
+    def median_of_three(array, left, right):
+        n = right - left + 1
+        mid = (left + right) // 2
+        
+        if array[left] > array[mid]:
+            if array[left] < array[right]:
+                return left
+            elif array[mid] > array[right]:
+                return mid
+            else:
+                return right
+        else:
+            if array[left] > array[right]:
+                return left
+            elif array[mid] < array[right]:
+                return mid
+            else:
+                return right
+       
 
     def partition(array, left, right):
+        count = right - left
+        #print('count: ', count)
         i = left + 1
-        print('left: ', left, 'right: ', right)
+        #print('left: ', left, 'right: ', right)
+        #array[left], array[right] = array[right], array[left]
+        median = median_of_three(array, left, right)
+        array[left], array[median] = array[median], array[left]
         pivot = array[left]
+        #print('Pivot: ', pivot)
         for j in range(left + 1, right + 1):
             if array[j] < pivot:
                 array[j], array[i] = array[i], array[j]
                 i += 1
         array[left], array[i - 1] = array[i - 1], array[left]
-        return i - 1
+        return i - 1, count
+    
+    count = 0
+    if left < right: 
+        partitioned = partition(array, left, right)
+        pivot_index = partitioned[0]
+        count = partitioned[1]
         
+        #print(array)
+        count += quicksort(array, left, pivot_index-1)
+        count += quicksort(array, pivot_index+1, right)
+        #print('Outer count: ', count)
         
 
-    pivot_index = partition(array, left, right)
-    print(array)
-    quicksort(array, left, pivot_index-1)
-    quicksort(array, pivot_index+1, right)
-    return array
+    return count
+    
 
+        
 
 def main():
-    print(quicksort([3, 8, 2, 5, 1, 4, 7, 6]))
+    with open('quicksort.txt') as fhand:
+        lst = fhand.read().split('\n')
+        sample = [int(entry) for entry in lst]
+        print('comparisons: ',quicksort(sample))
+        # print(sample[:100])
+        
 
 
 if __name__ == "__main__":
